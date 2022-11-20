1e10402229738118930e3b4986cb69aa98881373
AWOS <uws0331@gmail.com>
2022-11-20 13:58:40 +0900

    comment: recap super, inheritance, overide

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-class Human:
-    def __init__(self, name):
-        self.name = name
-
-    def say_hello(self):
-        print(f"hello my name is {self.name}")
+class Dog:
+    def woof(self):
+        print("woof woof")
 
 
-class Player(Human):
-
-    def __init__(self, name, exp):
-        super().__init__(name)
-        self.exp = exp
-
-
-class Fan(Human):
-    def __init__(self, name, fav_team):
-        super().__init__(name)
-        self.fav_team = fav_team
+class Beagle(Dog):
+    def woof(self):
+        super().woof()
+        print("super woof")
 
 
-awos_player = Player("awos", 10)
-awos_player.say_hello()
-awos_fan = Fan("awos_fan", "dontknow")
-awos_fan.say_hello()
+beagle = Beagle()
+beagle.woof()
