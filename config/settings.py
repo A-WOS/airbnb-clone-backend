d371d3fa8a9d06b16e3f2fbd4a63a9d78b3eccef
AWOS <uws0331@gmail.com>
2022-12-16 00:08:04 +0900

    feat: 커스텀 유저 모델 생성 및 유저 앱 추가

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 CUSTOM_APPS = [
     'houses.apps.HousesConfig',
+    'users.apps.UsersConfig',
 ]
 
 SYSTEM_APPS = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth
+AUTH_USER_MODEL = 'users.User'
