8af09cd8d849ce26c14f2728e03e2fd7d36d60ef
AWOS <uws0331@gmail.com>
2022-12-20 02:35:44 +0900

    feat: rooms 앱 생성 및 모델 생성, rooms 앱 등록

---
+++ rooms/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Room(models.Model):
+
+    """Room model Definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = ("shared_room", "Shared Room")
+
+    country = models.CharField(max_length=50, default="한국")
+    city = models.CharField(max_length=80, default="서울")
+    price = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=250)
+    pets_friendly = models.BooleanField(default=True)
+    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+
+class Amenity(models.Model):
+
+    """Amenity model Definition"""
+
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150, default="")
