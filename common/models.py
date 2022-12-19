72d894bbed3c1a319026eee6fc91091aad978d30
AWOS <uws0331@gmail.com>
2022-12-20 03:42:04 +0900

    comment: abstract model 주석 추가

--- common/models.py
+++ common/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class CommonModel(models.Model):
     """ Common Model Definition """
 
+    # DateTimeField -> 년, 날짜, 요일, 시, 분, 초
+    # DateField -> 날짜, 달, 년 만 저장.
+
+    # auto_now_add -> 처음 생성한 날짜 저장
+    # update 될 때마다 날짜 저장
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
