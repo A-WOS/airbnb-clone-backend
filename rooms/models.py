10bfbf0889d4ee0ba0d8cc2a5692627fdb89a66d
AWOS <uws0331@gmail.com>
2022-12-20 04:32:07 +0900

    feat: room 모델에 name field 추가 및 해당 모델의 객체명 부여

--- rooms/models.py
+++ rooms/models.py
@@ -3,7 +3,6 @@ from common.models import CommonModel
 
 
 class Room(CommonModel):
-
     """Room model Definition"""
 
     class RoomKindChoices(models.TextChoices):
@@ -11,6 +10,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -23,10 +23,21 @@ class Room(CommonModel):
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
     amenities = models.ManyToManyField("rooms.Amenity")
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Amenity(CommonModel):
-
     """Amenity model Definition"""
 
     name = models.CharField(max_length=150)
-    description = models.CharField(max_length=150, default="")
+    # default="" 대신에 null=True 로 작성 가능
+    # blank=True 는 Django form 에서 공백을 의미
+    # default="" 는 db 에서 null 을 의미
+    description = models.CharField(max_length=150, blank=True, default="")
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
