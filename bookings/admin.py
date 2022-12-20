2a9d2f32c470b590ea6dc4248fe361f122fa8e58
AWOS <uws0331@gmail.com>
2022-12-21 01:26:06 +0900

    feat: Bookings 앱 생성 및 모델 등록, admin 패널 커스터마이징

---
+++ bookings/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "kind",
+        "user",
+        "experience",
+        "check_in",
+        "check_out",
+        "experience_time",
+        "guests",
+    )
+
+    list_filter = (
+        "kind",
+    )
+
--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ CUSTOM_APPS = [
     'categories.apps.CategoriesConfig',
     'reviews.apps.ReviewsConfig',
     'wishlists.apps.WishlistsConfig',
+    'bookings.apps.BookingsConfig',
 ]
 
 SYSTEM_APPS = [
