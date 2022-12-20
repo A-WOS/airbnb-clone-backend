cc839467c54dbb034f057f4a843a256c0e22f3e4
AWOS <uws0331@gmail.com>
2022-12-21 05:59:33 +0900

    feat: reviews admin 패널에서 Custom Filter 생성

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,6 +3,27 @@ from django.contrib import admin
 from reviews.models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("thanks", "Thanks"),
+            ("awesome", "Awesome"),
+            ("별로", " 별로"),
+        ]
+
+    def queryset(self, request, reviews):
+        # print(request.GET['word']) == print(self.value)
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
@@ -11,5 +32,9 @@ class ReviewAdmin(admin.ModelAdmin):
     )
 
     list_filter = (
+        WordFilter,
         "rating",
+        "user__is_host",
+        "room__category",
+        "room__pets_friendly",
     )
