7926e7ae2812bbaf7103ebe91eb32a1463471e2c
AWOS <uws0331@gmail.com>
2022-12-22 04:41:31 +0900

    feat: 모든 방 보기, 하나의 방 보기 구현

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Room
 
-def say_hello(request):
-    return HttpResponse("hello")
+
+def see_all_rooms(request):
+    rooms = Room.objects.all()
+    context = {
+        "rooms": rooms,
+        "title": "Hello! this title comes from django!",
+    }
+    return render(request, "all_rooms.html", context)
+
+
+def see_one_room(request, room_id):
+    # room = Room.objects.get(pk=room_id)
+    room = get_object_or_404(Room, pk=room_id)
+    context = {
+        "room": room,
+    }
+    return render(request, "room_detail.html", context)
