c3d2c93a931c1f57f2a3aa4ccfa8a6123bd4c04c
AWOS <uws0331@gmail.com>
2022-12-23 03:00:18 +0900

    feat: querySet 을 json 타입으로 변경

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Category
+from django.http import JsonResponse
+from django.core import serializers
 
-# Create your views here.
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse(
+        {
+            "ok": True,
+            # serializers = querySet 을 json 타입으로 변경
+            "categories": serializers.serialize("json", all_categories),
+        }
+    )
