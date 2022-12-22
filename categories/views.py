31b28ef5ed830713c36c4d712ecfcf079d973dba
AWOS <uws0331@gmail.com>
2022-12-23 03:34:03 +0900

    feat: POST 기능 추가

--- categories/views.py
+++ categories/views.py
@@ -4,14 +4,20 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    # 여러 개의 카테고리를 보내고 싶다면 many=True 옵션 추가
-    serializer = CategorySerializer(all_categories, many=True)
-    return Response(
-        {
-            "ok": True,
-            "categories": serializer.data,
-        }
-    )
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        # 여러 개의 카테고리를 보내고 싶다면 many=True 옵션 추가
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
